eacf99fba778008f637e5e45c30b2b52db92497d
Mohd Aman <saifiamaan445@gmail.com>
2023-06-20 07:55:27 +0530

    async defer

---
+++ module2/class19/script.js
@@ -0,0 +1,11 @@
+let div = document.createElement("div");
+div.innerText = "I am a div";
+let body = document.querySelector("body");
+console.log(body);
+body.appendChild(div);
+
+// let count =0;
+// while(count<100000){
+//     console.log(count);
+//     count++;
+// }
