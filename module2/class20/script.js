9d01467ac245daf180c8f637c24aa574792edde1
Mohd Aman <saifiamaan445@gmail.com>
2023-06-22 07:57:22 +0530

    this in node

---
+++ module2/class20/script.js
@@ -0,0 +1,7 @@
+"use strict"
+
+// let num = 10;
+// console.log(num);
+
+a = 20;  // declaring a variable in non strict mode
+console.log(a);
---
+++ module2/class20/thisInNode.js
@@ -0,0 +1,51 @@
+// console.log(this);    // {} empty object
+
+
+// function fxn(){
+//     console.log(this);  // global objecct
+// }
+
+// fxn();
+
+// function fxn(){
+//     function gxn(){
+//         console.log(this);    // global object
+//     }
+//     gxn();
+// }
+
+// fxn();
+
+// let obj = {
+//     name:"Siba",
+//     age:"No idea",
+//     sayHello:function(){
+//         console.log(this);
+//     }
+// }
+
+// obj.sayHello();         /// object itself {name:Siba,age:"No idea", sayHello:function}
+
+// let sayHello1 = obj.sayHello;
+
+// //this is same as line number 29 
+// // let sayHello1 = function(){
+// //     console.log(this);
+// // }
+
+// sayHello1();    /// global object
+
+
+let anotherObject = {
+    name:"Vivek",
+    age:"Don't know",
+    sayHello:function(){
+        console.log("value of this inside sayHello: ", this)
+        function gxn(){
+            console.log("value of this inside gxn: ", this);
+        }
+        gxn();
+    }
+}
+
+anotherObject.sayHello();
