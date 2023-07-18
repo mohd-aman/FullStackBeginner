0578f120334d29e2b9832be72da31ca0d9d3694f
Mohd Aman <saifiamaan445@gmail.com>
2023-07-18 08:11:30 +0530

    form

--- module3/react-basics/my-app/src/Components/Form.jsx
+++ module3/react-basics/my-app/src/Components/Form.jsx
@@ -2,22 +2,32 @@ import { useState } from "react"
 
  export default function Form(){
      let [firstName,setFirstName] = useState("");
+     let [lastName,setLastName] = useState("");
 
-     let handleChange = (e)=>{
+     let handleFirstName = (e)=>{
         // console.log(e); // Native Event
         // console.log(e.target.value);
         setFirstName(e.target.value);
+     }
 
+     let handleLastName = (e)=>{
+         setLastName(e.target.value);
+     }
+
+     let handleSubmit = (e)=>{
+        e.preventDefault();
+         console.log(firstName,lastName);
      }
 
      return(
          <div>
              <h1>Form</h1>
-             <form>
+             <form onSubmit={handleSubmit}>
                  <label> First Name : </label>
-                 <input onChange={handleChange} type="text" value={firstName} />
+                 <input onChange={handleFirstName} type="text" value={firstName} />
                  <label>Last Name : </label>
-                 <input type="text" value={"last name"} />
+                 <input  onChange = {handleLastName} type="text" value={lastName} />
+                 <button type="submit">Submit</button>
              </form>
          </div>
      )
