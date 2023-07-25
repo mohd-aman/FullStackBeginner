bc7c59b5491fdfe12037107d743df4790eb8eb7e
Mohd Aman <saifiamaan445@gmail.com>
2023-07-25 08:10:21 +0530

    get banner movie dynamically

--- module3/movies-app/src/Components/Banner.jsx
+++ module3/movies-app/src/Components/Banner.jsx
@@ -1,11 +1,31 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
 
 export default function Banner(){
+    let [movieObj,setMovieObj] = useState({});
+
+
+    useEffect(()=>{
+        axios.get("https://api.themoviedb.org/3/trending/movie/day?api_key=2816c138913c6ef73d40c883d36fbe56")
+        .then(function(response){
+        // console.log(response.data.results);
+            let movies = response.data.results;
+            let randomMovie = movies[Math.floor(20*Math.random())];
+            console.log(randomMovie);
+            setMovieObj(randomMovie);
+    })
+    },[])
+
+    if(movieObj.poster_path == undefined){
+        return <>Loading....</>
+    }
+
     return(
         <div className="h-[70vh] bg-cover bg-center flex items-end "
-            style={{backgroundImage:`url(https://assets-in.bmscdn.com/discovery-catalog/events/et00311714-ewdhvajezf-landscape.jpg)`}}
+            style={{backgroundImage:`url(https://image.tmdb.org/t/p/original${movieObj.poster_path})`}}
         >
             <div className="bg-stone-900/60 w-full text-center p-4 text-2xl text-white">
-                John Wick
+                {movieObj.original_title}
             </div>
         </div>
     )
