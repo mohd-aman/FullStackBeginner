0f579ba6628ef67998f60fbf42e8ff513bd44cd4
Mohd Aman <saifiamaan445@gmail.com>
2023-08-24 07:54:09 +0530

    put post delete method and postman

--- module4/class2/server.js
+++ module4/class2/server.js
@@ -4,12 +4,23 @@ const app = express();
 
 //endpoint
 app.get("/",(req,res)=>{
-    res.send("Hello World");
+    // res.send("Hello World");
     // res.send("<h1>Hello world in h1 heading</h1")
     // res.send({"name":"Deepanshu"})
-
+    res.send({'type':"GET"})
 })
 
+app.post("/",(req,res)=>{
+    res.send({"type":"POST"});
+})
+
+app.put("/",(req,res)=>{
+    res.send({"type":"PUT"})
+})
+
+app.delete("/",(req,res)=>{
+    res.send({"type":"DELETE"})
+})
 
 app.listen(8082,()=>{
     console.log("Server has started at 8080")
