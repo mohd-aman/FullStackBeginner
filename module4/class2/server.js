82c55ca4a57b6ffb32d5e26c487810d46c6fccb3
Mohd Aman <saifiamaan445@gmail.com>
2023-08-24 09:04:11 +0530

    route levelmiddleware and built in middleware

--- module4/class2/server.js
+++ module4/class2/server.js
@@ -1,16 +1,49 @@
 const express = require("express");
 
 const app = express();
+const PORT = 8080
+
+
+//Built in middleware
+app.use(express.static('public'))
+
+
+//I want to create a logger middleware here
+// app.use((req,res,next)=>{
+//     console.log(req.method,req.ip,req.hostname, new Date());
+//     next();
+// })
+
+const auth = (req,res,next)=>{
+    console.log(req.query)
+    let password = req.query.password;
+    if(password == '123'){
+        next();
+    }else{
+        res.send("You are not authorized person");
+    }
+}
+
+// app.use(auth);  // app.use method is used to invoke middlewares
+
+//Route Level Middlewares
 
 //endpoint
-app.get("/",(req,res)=>{
+app.get('/',(req,res)=>{
+    res.send({"type":"get"})
+})
+
+app.get("/users/:id", auth, (req,res)=>{
+    console.log(req.params);
     // res.send("Hello World");
     // res.send("<h1>Hello world in h1 heading</h1")
     // res.send({"name":"Deepanshu"})
-    res.send({'type':"GET"})
+    res.send("Authorized person access granted")
 })
 
-app.post("/",(req,res)=>{
+
+
+app.post("/", (req,res)=>{
     res.send({"type":"POST"});
 })
 
@@ -22,6 +55,6 @@ app.delete("/",(req,res)=>{
     res.send({"type":"DELETE"})
 })
 
-app.listen(8082,()=>{
-    console.log("Server has started at 8080")
+app.listen(PORT,()=>{
+    console.log(`Server has started at ${PORT}`)
 })
