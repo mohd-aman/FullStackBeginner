6d8831c63701c0d1b5c5c3e28b82b4bbcab28cfb
Mohd Aman <saifiamaan445@gmail.com>
2023-08-29 08:21:02 +0530

    create

--- module4/class3 and 4/index.js
+++ module4/class3 and 4/index.js
@@ -11,3 +11,31 @@ mongoose.connect(db, )
 .catch((err)=>{
     console.log(err);
 })
+
+//Schema
+
+const courseSchema = new mongoose.Schema({
+    name:String,
+    creator:String,
+    publishedDate:{type:Date,default:Date.now},
+    isPublished:Boolean,
+    rating:Number
+})
+
+//Model
+
+const Course = mongoose.model('Course',courseSchema);
+
+
+//CRUD
+async function createCourse(){
+    const course = new Course({
+        name:"C++",
+        creator:"XYZ",
+        isPublished:true,
+        rating:3.8
+    })
+    const courseCreated = await course.save();
+}
+
+// createCourse();
