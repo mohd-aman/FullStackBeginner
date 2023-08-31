f0847727cb21c4a551230a411698725d20ef3485
Mohd Aman <saifiamaan445@gmail.com>
2023-08-31 08:31:48 +0530

    register a user

--- module4/class3 and 4/index.js
+++ module4/class3 and 4/index.js
@@ -4,7 +4,7 @@ const password = "odWbGMYr44bZOwaC";
 
 const db = `mongodb+srv://saifiamaan445:${password}@cluster0.umc6xfk.mongodb.net/?retryWrites=true&w=majority`
 
-mongoose.connect(db, )
+mongoose.connect(db )
 .then(()=>{
     console.log("Connection Established")
 })
