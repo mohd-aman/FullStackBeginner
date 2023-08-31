f733d02f7e2635f5e40156c77d036a11c61913fe
Mohd Aman <saifiamaan445@gmail.com>
2023-08-31 09:20:43 +0530

    login and register pages done

--- module4/bookMyShow/client/src/App.js
+++ module4/bookMyShow/client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 import './App.css';
+
+import './stylesheets/alignments.css'
+import './stylesheets/sizes.css'
+import './stylesheets/form-elements.css'
+import './stylesheets/theme.css'
+import './stylesheets/custom.css'
+
+
+import Home from "./pages/Home"
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
