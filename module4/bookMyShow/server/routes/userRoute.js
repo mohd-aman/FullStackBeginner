fa07088c26c5e91eb29182cf8a9c3b15de3916ce
Mohd Aman <saifiamaan445@gmail.com>
2023-08-31 08:52:27 +0530

    login route

--- module4/bookMyShow/server/routes/userRoute.js
+++ module4/bookMyShow/server/routes/userRoute.js
@@ -22,11 +22,45 @@ router.post('/register',async (req,res)=>{
         const newUser = new User(req.body);
         console.log(newUser)
         await newUser.save();
-        res.send("got the data")
+        res.send({
+            success:true,
+            message:"User Registered, Now please login"
+        })
     }catch(err){
         console.log(err);    
     }
     
+})
+
+router.post('/login',async (req,res)=>{
+    try{
+        const user = await User.findOne({email:req.body.email});
+        if(!user){
+            return res.send({
+                success:false,
+                message:"User with this email does not exists"
+            })
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+        if(!validPassword){
+            return res.send({
+                success:false,
+                message:"Invalid Password"
+            })
+        }
+
+        res.send({
+            success:true,
+            message:"User Logged In"
+        })
+
+    }catch(err){
+        console.log(err);
+    }
+    
+
 })
 
 module.exports = router;
